59de4040beadf799d50584f6736552237dc9e763
gray <1012518214@qq.com>
2021-11-29 17:50:44 +0800

    init

---
+++ .vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: "干爆前端",
+  description: "基于云开发的 VuePress 网站应用示例",
+  base: "/vuepress/",
+  themeConfig: {
+    nav: [{
+        text: "首页",
+        link: "/"
+      },
+      {
+        text: "指南",
+        link: "/guides/"
+      },
+      {
+        text: "leetcode",
+        link: "/leetcode/"
+      }
+    ],
+  },
+};
